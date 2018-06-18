6798b1e6b6a50a202887b856d9f84b12fcd5b918
Jakub <nawias@gmail.com>
2018-06-18 07:13:54 +0200

    03/statyczny-komponent-aplikacji

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => (
+  <div>
+    <div className="countdown">
+      <strong>śniadanie</strong> - 07:00
+    </div>
+    <div className="countdown">
+      <strong>obiad</strong> - 15:00
+    </div>
+  </div>
+);
+
+export default App;
--- src/Hello.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Hello = () => <h1>Hello World</h1>;
-
-export default Hello;
