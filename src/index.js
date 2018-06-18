5907da2ea4a720b125c47ce0d6b830c60ade7a3f
Jakub <nawias@gmail.com>
2018-06-18 06:11:49 +0200

    upraszczamy projekt

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Hello from "./Hello";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Hello />, document.getElementById("root"));
