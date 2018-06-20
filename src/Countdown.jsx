fb1c9aabef5aebe1274adc94690777c49b5c8e87
Jakub <nawias@gmail.com>
2018-06-20 18:06:12 +0200

    proptypes czyli typy danych

--- src/Countdown.jsx
+++ src/Countdown.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Countdown = props => (
   <div className="countdown">
     <strong>{props.name}</strong> - {props.time}
   </div>
 );
+
+Countdown.propTypes = {
+  name: PropTypes.string,
+  time: PropTypes.string
+};
 
 export default Countdown;
