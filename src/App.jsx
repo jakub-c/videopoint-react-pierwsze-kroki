842892b560e5dd445c8a4b98b4f9bf801332a390
Jakub <nawias@gmail.com>
2018-06-21 06:53:17 +0200

    komponenty stanu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Countdown from "./Countdown";
 
-const App = () => (
-  <div>
-    <Countdown name="śniadanie" time="07:00" />
-    <Countdown name="obiad" time="15:00" />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [
+        { id: 0, name: "śniadanie", time: "07:00" },
+        { id: 1, name: "obiad", time: "15:00" }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Countdown name="śniadanie" time="07:00" />
+        <Countdown name="obiad" time="15:00" />
+      </div>
+    );
+  }
+}
 
 export default App;
